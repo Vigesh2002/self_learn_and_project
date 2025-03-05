bd4708f4ab451637cefe9f3a8305043b7eb3e3bf
Vigesh <robovigesh987654321@gmail.com>
2025-03-05 11:49:15 +0530

    Work done

---
+++ ReactJs/react_course_learning/src/Components/01.Fundamentals/Dynamic Content/Index1.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Greeting from './components/Greeting'
+import ProductInfo from './components/ProductInfo'
+
+const Index1 = () => {
+  return (
+    <div>
+      <Greeting/>
+      <ProductInfo/>
+    </div>
+  )
+}
+
+export default Index1
